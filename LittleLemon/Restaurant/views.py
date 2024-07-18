bc1e4e69ea228666a3c6f9471626e9ce542bd138
phucnn <phucnn@vng.com.vn>
2024-07-18 15:05:21 +0700

    another commit

--- LittleLemon/Restaurant/views.py
+++ LittleLemon/Restaurant/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def sayhello(request):
+    return HttpResponse("Hello Django!")
+
+
+def index(request):
+    return render(request, 'index.html')
