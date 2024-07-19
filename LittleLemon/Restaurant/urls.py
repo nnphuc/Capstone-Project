0cbef806a543b8c16f31345775c0453c8b230516
phucnn <phucnn@vng.com.vn>
2024-07-19 10:49:16 +0700

    add route booking

--- LittleLemon/Restaurant/urls.py
+++ LittleLemon/Restaurant/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 from . import views
 
@@ -9,4 +10,5 @@ urlpatterns = [
     # add following lines to urlpatterns list
     path('menu/', views.MenuItemView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
